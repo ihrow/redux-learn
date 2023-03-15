56f09e112edf5099087c292b108bf69ad2243f71
igor <igornotfound@gmail.com>
2023-03-16 00:28:23 +0100

    adding / removing clients

--- src/store/clientReducer.js
+++ src/store/clientReducer.js
@@ -1,13 +1,13 @@
 const defaultState = {
-  customers: []
+  clients: []
 }
 
 export const clientReducer = (state = defaultState, action) => {
   switch (action.type) {
     case "ADD_CLIENT":
-      return {...state, customers: [...state.customers, action.payload]};
+      return {...state, clients: [...state.clients, action.payload]};
     case "REMOVE_CLIENT":
-      return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)};
+      return {...state, clients: state.clients.filter(customer => customer.name !== action.payload.name)};
     default:
       return state;
   }
